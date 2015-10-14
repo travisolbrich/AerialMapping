ba9d47bc156d96e19251b65fffc830702cfd3487
Taylor Hunt <thehuntclub@tamu.edu>
2015-10-14 15:51:44 -0500

    Added unit testing framework

---
+++ Project/AerialMappingTests/AerialMappingTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AerialMappingTests
+{
+    [TestClass]
+    public class AerialMappingTest1
+    {
+        [TestMethod]
+        public void LoadKML1()
+        {
+            
+        }
+    }
+}
