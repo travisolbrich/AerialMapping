68e23d550c2b31fa845588e1e9aaa51f13fee33b
Taylor Hunt <thehuntclub@tamu.edu>
2015-10-24 19:44:22 -0500

    Attempted to add MVVM to work better with bindings

--- Project/AerialMapping/MainWindow.xaml.cs
+++ Project/AerialMapping/MainWindow.xaml.cs
@@ -28,38 +28,42 @@ namespace AerialMapping
     {
         public double m_KmzZoomDelaySec = 3;
         private double currAngle = 0;
-        private string m_IdToZoomOn = "";
-        private MapView m_MapView;
+        //private string m_IdToZoomOn = "";
+        //private MapView m_MapView;
         private Viewpoint m_CenterPoint;
         KmlLayer kmllayerTest;
         private List<Dataset> m_DatasetList;
-
+        
+        MainViewModel mainViewModel;
 
         public MainWindow()
         {
+            mainViewModel = new MainViewModel();
+            this.DataContext = mainViewModel;
             InitializeComponent();
-            m_MapView.MaxScale = 2100; // set the maximum zoom value
+            
+            //mainViewModel.m_MapView.MaxScale = 2100; // set the maximum zoom value
             m_DatasetList = new List<Dataset>();
         }
 
-        public void LoadKml(string path, bool bZoomTo, bool bRelativePath)
-        {
-            Debug.WriteLine("Path: " + path);
-            try
-            {
-                Uri dataPath = new Uri(path, bRelativePath ? UriKind.Relative : UriKind.Absolute);
-                KmlLayer kmllayer = new KmlLayer(dataPath);
-                kmllayer.ID = path;
-                m_IdToZoomOn = bZoomTo ? path : "";
+        //public void LoadKml(string path, bool bZoomTo, bool bRelativePath)
+        //{
+        //    Debug.WriteLine("Path: " + path);
+        //    try
+        //    {
+        //        Uri dataPath = new Uri(path, bRelativePath ? UriKind.Relative : UriKind.Absolute);
+        //        KmlLayer kmllayer = new KmlLayer(dataPath);
+        //        kmllayer.ID = path;
+        //        m_IdToZoomOn = bZoomTo ? path : "";
 
-                m_MapView.Map.Layers.Add(kmllayer);
-                kmllayerTest = kmllayer;
-            }
-            catch
-            {
-                Debug.WriteLine(string.Format("(MainWindows{LoadKml}) Could not load KML with path {0}", path));
-            }
-        }
+        //        mainViewModel.m_MapView.Map.Layers.Add(kmllayer);
+        //        kmllayerTest = kmllayer;
+        //    }
+        //    catch
+        //    {
+        //        Debug.WriteLine(string.Format("(MainWindows{LoadKml}) Could not load KML with path {0}", path));
+        //    }
+        //}
 
         private void BaseMapView_LayerLoaded(object sender, LayerLoadedEventArgs e)
         {
@@ -68,9 +72,9 @@ namespace AerialMapping
                 Debug.WriteLine(string.Format("(MainWindows{BaseMapView_LayerLoaded}) Error while loading layer : {0} - {1}", e.Layer.ID, e.LoadError.Message));
                 return;
             }
-            if (m_IdToZoomOn != "" && e.Layer.ID == m_IdToZoomOn)
+            if (mainViewModel.m_IdToZoomOn != "" && e.Layer.ID == mainViewModel.m_IdToZoomOn)
             {
-                m_IdToZoomOn = "";
+                mainViewModel.m_IdToZoomOn = "";
                 m_CenterPoint = ((KmlLayer)e.Layer).RootFeature.Viewpoint;
                 ((MapView)sender).SetViewAsync(m_CenterPoint, TimeSpan.FromSeconds(m_KmzZoomDelaySec));
             }
@@ -79,8 +83,9 @@ namespace AerialMapping
 
         private void BaseMapView_Initialized(object sender, EventArgs e)
         {
-            m_MapView = (MapView)sender;
-            LoadKml("../../../../Data/SampleOne/TestData.kml", true, true);
+            mainViewModel.m_MapView = (MapView)sender;
+            mainViewModel.m_MapView.MaxScale = 2100; // set the maximum zoom value
+            mainViewModel.LoadKml("../../../../Data/SampleOne/TestData.kml", true, true);
         }
 
         private void bOpenFileDialog_Click(object sender, RoutedEventArgs e)
@@ -90,7 +95,7 @@ namespace AerialMapping
             if (openFileDialog.ShowDialog() == true)
             {
                 string filePath = openFileDialog.FileName;
-                LoadKml(filePath, true, false);
+                mainViewModel.LoadKml(filePath, true, false);
             }
         }
 
@@ -104,55 +109,60 @@ namespace AerialMapping
         {
             currAngle = (currAngle += 10) % 360;
             // apply the rotation to the map
-            m_MapView.SetRotationAsync(currAngle);
+            mainViewModel.m_MapView.SetRotationAsync(currAngle);
         }
 
         private void bRotateDefault_Click(object sender, RoutedEventArgs e)
         {
             // apply the rotation to the map
             currAngle = 0;
-            m_MapView.SetRotationAsync(currAngle);
+            mainViewModel.m_MapView.SetRotationAsync(currAngle);
         }
         private void bRotateRight_Click(object sender, RoutedEventArgs e)
         {
             currAngle = (currAngle -= 10) % 360;
             // apply the rotation to the map
-            m_MapView.SetRotationAsync(currAngle);
+            mainViewModel.m_MapView.SetRotationAsync(currAngle);
         }
 
         // The following are buttons for zoom
         private void bZoomIn_Click(object sender, RoutedEventArgs e)
         {
-            m_MapView.ZoomAsync(1.2);
-            bZoomSlider.Value = m_MapView.Scale;
+            mainViewModel.m_MapView.ZoomAsync(1.2);
+            bZoomSlider.Value = mainViewModel.m_MapView.Scale;
         }
 
         private void bZoomOut_Click(object sender, RoutedEventArgs e)
         {
-            m_MapView.ZoomAsync(0.8);
-            bZoomSlider.Value = m_MapView.Scale;
+            mainViewModel.m_MapView.ZoomAsync(0.8);
+            bZoomSlider.Value = mainViewModel.m_MapView.Scale;
         }
 
         // The following is for zoom slider action
         private void bZoomSlider_Click(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             double zoomScale = e.NewValue;
-            m_MapView.ZoomToScaleAsync(zoomScale);
+            if (mainViewModel != null && mainViewModel.m_MapView != null)
+            {
+                mainViewModel.m_MapView.ZoomToScaleAsync(zoomScale);
+            }
+            
         }
 
         public void AddLayerToTree(Layer layer)
         {
             MenuItem root = new MenuItem() { Title = "Location" };
             root.Items.Add(new MenuItem() { Title = "Time" });
-            LayerView.Items.Add(root);
+            //TreeViewItems.Add(root);
+            //LayerView.Items.Add(root);
         }
 
         // The following is for the time slider
         private void bTimeSlider_Click(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             // this is for testing only.
-            if (e.NewValue == 3) m_MapView.Map.Layers.Add(kmllayerTest);
-            else if (e.NewValue == 2) m_MapView.Map.Layers.Remove(kmllayerTest);
+            if (e.NewValue == 3) mainViewModel.m_MapView.Map.Layers.Add(kmllayerTest);
+            else if (e.NewValue == 2) mainViewModel.m_MapView.Map.Layers.Remove(kmllayerTest);
             // m_MapView.Map.Layers.Move(layerVectorThing[e.NewValue], 0);
         }
 
@@ -166,7 +176,7 @@ namespace AerialMapping
         private void bCenterView_Click(object sender, RoutedEventArgs e)
         {
             //((MapView)sender).SetViewAsync(((KmlLayer)e.Layer).RootFeature.Viewpoint, TimeSpan.FromSeconds(m_KmzZoomDelaySec));
-            m_MapView.SetViewAsync(m_CenterPoint);
+            mainViewModel.m_MapView.SetViewAsync(m_CenterPoint);
         }
 
 
@@ -198,34 +208,42 @@ namespace AerialMapping
         // This pops up an "Add Layer" window and gathers the appropriate data
         // from the user. The new layer is then added to the "layers" treeview
         // and loaded into the map.
-        private void bAddLayer_Click(object sender, RoutedEventArgs e)
+        //private void bAddLayer_Click(object sender, RoutedEventArgs e)
+        //{
+        //    // Popup a window to get the layer information
+        //    AddLayer addLayer = new AddLayer();
+        //    addLayer.Owner = this;
+        //    addLayer.ShowDialog();
+            
+        //    Dataset newLayer = addLayer.DatasetToAdd;
+        //    addLayer.Close();
+
+        //    if (!String.IsNullOrEmpty(newLayer.FilePath))
+        //    {
+        //        // Save the new dataset
+        //        m_DatasetList.Add(newLayer);
+
+        //        // Add it to the TreeView on the UI
+        //        MenuItem root = new MenuItem() { Title = newLayer.Location };
+        //        root.Items.Add(new MenuItem() { Title = newLayer.Time.ToShortDateString() });
+        //        mainViewModel.TreeViewItems.Add(root);
+
+        //        // Open the new layer
+        //        LoadKml(newLayer.FilePath, true, false);
+        //    }
+
+        //    Debug.WriteLine("Location: " + newLayer.Location);
+        //    Debug.WriteLine("Time: " + newLayer.Time);
+        //    Debug.WriteLine("File Path: " + newLayer.FilePath);
+            
+        //}
+
+        private void bRemoveLayer_Click(object sender, RoutedEventArgs e)
         {
-            // Popup a window to get the layer information
-            AddLayer addLayer = new AddLayer();
-            addLayer.Owner = this;
-            addLayer.ShowDialog();
-            
-            Dataset newLayer = addLayer.DatasetToAdd;
-            addLayer.Close();
+            //LayerView.Items.
 
-            if (!String.IsNullOrEmpty(newLayer.FilePath))
-            {
-                // Save the new dataset
-                m_DatasetList.Add(newLayer);
-
-                // Add it to the TreeView on the UI
-                MenuItem root = new MenuItem() { Title = newLayer.Location };
-                root.Items.Add(new MenuItem() { Title = newLayer.Time.ToShortDateString() });
-                LayerView.Items.Add(root);
-
-                // Open the new layer
-                LoadKml(newLayer.FilePath, true, false);
-            }
-
-            Debug.WriteLine("Location: " + newLayer.Location);
-            Debug.WriteLine("Time: " + newLayer.Time);
-            Debug.WriteLine("File Path: " + newLayer.FilePath);
-            
+            RemoveLayers removeLayers = new RemoveLayers();
+            removeLayers.ShowDialog();
         }
     }
 }
---
+++ Project/AerialMapping/RemoveLayers.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace AerialMapping
+{
+    /// <summary>
+    /// Interaction logic for RemoveLayers.xaml
+    /// </summary>
+    public partial class RemoveLayers : Window
+    {
+        
+        ObservableCollection<CustomItem> OC { get; set; }
+        ObservableCollection<CustomItem> ChildOC { get; set; }
+        public RemoveLayers()
+        {
+            OC = new ObservableCollection<CustomItem>()
+            {
+                new CustomItem(){Name="Item1", Checked=true,
+                  Children = new ObservableCollection<CustomItem>()
+                  {            
+                    new CustomItem(){Name="SubItem11", Checked=false},
+                    new CustomItem(){Name="SubItem12", Checked=false},
+                    new CustomItem(){Name="SubItem13", Checked=false}
+                  }
+                },
+                new CustomItem(){Name="Item2", Checked=true,
+                  Children = new ObservableCollection<CustomItem>()
+                  {
+                    new CustomItem(){Name="SubItem21", Checked=true},            
+                    new CustomItem(){Name="SubItem22", Checked=true},
+                    new CustomItem(){Name="SubItem23", Checked=true}
+                  }},
+                new CustomItem(){Name="Item3", Checked=true,
+                  Children = new ObservableCollection<CustomItem>()
+                  {
+                    new CustomItem(){Name="SubItem31", Checked=false},
+                    new CustomItem(){Name="SubItem32", Checked=false},            
+                    new CustomItem(){Name="SubItem33", Checked=false}
+                  }},
+                new CustomItem(){Name="Item4", Checked=true,
+                  Children = new ObservableCollection<CustomItem>()
+                  {
+                    new CustomItem(){Name="SubItem41", Checked=false},
+                    new CustomItem(){Name="SubItem42", Checked=false},
+                    new CustomItem(){Name="SubItem43", Checked=false}
+                  }
+                }
+              };
+              InitializeComponent();
+              this.DataContext = OC;
+        }
+
+        public void OnCheck()
+        {
+            ChildOC = new ObservableCollection<CustomItem>() { };
+            foreach (CustomItem item in OC)
+            {
+                if (item.Checked == true)
+                {
+                    ChildOC.Add(item);
+                    foreach (CustomItem subitem in item.Children)
+                    {
+                        if (subitem.Checked == true)
+                        {
+                            ChildOC.Add(subitem);
+                        }
+                    }         
+                }
+            }
+            listbox.ItemsSource = ChildOC;
+        }
+        private void CheckBox_Click(object sender, RoutedEventArgs e) { OnCheck(); }
+        private void CheckBox_Loaded(object sender, RoutedEventArgs e) { OnCheck(); }
+    }
+    public class CustomItem
+    {
+        public string Name { get; set; }
+        public bool Checked { get; set; }
+        public ObservableCollection<CustomItem> Children { get; set; }
+    }
+}
+
